c9753dc8baa7b4a4db54cecf226d4673f67a8fa6
nathangtg <nathanginting2007@gmail.com>
2025-09-17 03:51:40 +0800

    Add fetchApiMockProvider to ChatIntroComponent tests for improved API interaction

--- agent-hub/src/app/chat/shared/chat-intro/chat-intro.component.spec.ts
+++ agent-hub/src/app/chat/shared/chat-intro/chat-intro.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatIntroComponent } from './chat-intro.component';
+import { fetchApiMockProvider } from '../../../shared/utils/fetch-mock-provider.test-util';
 
 describe('ChatIntroComponent', () => {
   let component: ChatIntroComponent;
@@ -8,9 +9,10 @@ describe('ChatIntroComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChatIntroComponent]
+      imports: [ChatIntroComponent],
+      providers: [fetchApiMockProvider]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ChatIntroComponent);
     component = fixture.componentInstance;
