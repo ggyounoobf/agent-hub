e29e441156c373ce889f8dd14a3f38ce3b1c0be7
nathangtg <nathanginting2007@gmail.com>
2025-09-17 03:52:06 +0800

    Fix import path for Chat model in ConfirmDeleteModalComponent tests

--- agent-hub/src/app/shared/sidebar/chat-link/confirm-delete-modal/confirm-delete-modal.component.spec.ts
+++ agent-hub/src/app/shared/sidebar/chat-link/confirm-delete-modal/confirm-delete-modal.component.spec.ts
@@ -1,10 +1,10 @@
+import { Chat } from './../../../../../model/chat';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { signal } from '@angular/core';
 import { List } from 'immutable';
 import { MODAL_DATA, ModalController } from '@ngx-templates/shared/modal';
 
 import { ConfirmDeleteModalComponent } from './confirm-delete-modal.component';
-import { Chat } from '../../../../model';
 
 describe('ConfirmDeleteModalComponent', () => {
   let component: ConfirmDeleteModalComponent;
